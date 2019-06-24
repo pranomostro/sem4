6851cd52ff637f34bfb6d2231c2eb7955618657c
pranomostro <pranomostro@posteo.net>
2019-06-24 15:32:47 +0200

    A, B in Theo Hausaufgabe.

---
+++ theo_ha/8/A/palind.py
@@ -0,0 +1,39 @@
+s = """01NZ
+q0
+q8
+q0; ;q1; ;L
+q0;0;q0;0;R
+q0;1;q0;1;R
+q1; ;q6; ;R
+q1;0;q2;Z;R
+q1;1;q3;N;R
+q1;N;q1;N;L
+q1;Z;q1;Z;L
+q2; ;q4;0;L
+q2;0;q2;0;R
+q2;1;q2;1;R
+q2;N;q2;N;R
+q2;Z;q2;Z;R
+q3; ;q5;1;L
+q3;0;q3;0;R
+q3;1;q3;1;R
+q3;N;q3;N;R
+q3;Z;q3;Z;R
+q4;0;q4;0;L
+q4;1;q4;1;L
+q4;N;q1;N;N
+q4;Z;q1;Z;N
+q5;0;q5;0;L
+q5;1;q5;1;L
+q5;N;q1;N;N
+q5;Z;q1;Z;N
+q6; ;q8; ;N
+q6;0;q7;0;L
+q6;1;q7;1;L
+q6;N;q6;1;R
+q6;Z;q6;0;R
+q7; ;q8; ;R
+q7;0;q7;0;L
+q7;1;q7;1;L
+END"""
+print(s)
---
+++ theo_ha/8/B/parens.py
@@ -0,0 +1,102 @@
+s = """#()ABCD[]
+q0
+qf
+q0; ;q1;#;L
+q0;(;q0;(;R
+q0;);q0;);R
+q0;[;q0;[;R
+q0;];q0;];R
+q10;B;q11; ;L
+q11; ;q1; ;N
+q11;#;q11;#;L
+q11;(;q1;(;N
+q11;);q1;);N
+q11;A;q11;A;L
+q11;B;q11;B;L
+q11;C;q11;C;L
+q11;D;q11;D;L
+q11;[;q1;[;N
+q11;];q1;];N
+q12;#;q13;#;R
+q12;A;q12;A;R
+q12;B;q12;B;R
+q12;C;q12;C;R
+q12;D;q12;D;R
+q13; ;qf; ;N
+q1; ;q12; ;R
+q1;(;q9;D;R
+q1;);q4;B;R
+q1;[;q6;C;R
+q1;];q2;A;R
+q2; ;q3;A;L
+q2;#;q2;#;R
+q2;(;q2;(;R
+q2;);q2;);R
+q2;A;q2;A;R
+q2;B;q2;B;R
+q2;C;q2;C;R
+q2;D;q2;D;R
+q2;[;q2;[;R
+q2;];q2;];R
+q3;#;q3;#;L
+q3;(;q1;(;N
+q3;);q1;);N
+q3;A;q3;A;L
+q3;B;q3;B;L
+q3;C;q3;C;L
+q3;D;q3;D;L
+q3;[;q1;[;N
+q3;];q1;];N
+q4; ;q5;B;L
+q4;#;q4;#;R
+q4;(;q4;(;R
+q4;);q4;);R
+q4;A;q4;A;R
+q4;B;q4;B;R
+q4;C;q4;C;R
+q4;D;q4;D;R
+q4;[;q4;[;R
+q4;];q4;];R
+q5;#;q5;#;L
+q5;(;q1;(;N
+q5;);q1;);N
+q5;A;q5;A;L
+q5;B;q5;B;L
+q5;C;q5;C;L
+q5;D;q5;D;L
+q5;[;q1;[;N
+q5;];q1;];N
+q6; ;q7; ;L
+q6;#;q6;#;R
+q6;(;q6;(;R
+q6;);q6;);R
+q6;A;q6;A;R
+q6;B;q6;B;R
+q6;C;q6;C;R
+q6;D;q6;D;R
+q6;[;q6;[;R
+q6;];q6;];R
+q7;A;q8; ;L
+q8; ;q1; ;N
+q8;#;q8;#;L
+q8;(;q1;(;N
+q8;);q1;);N
+q8;A;q8;A;L
+q8;B;q8;B;L
+q8;C;q8;C;L
+q8;D;q8;D;L
+q8;[;q1;[;N
+q8;];q1;];N
+q9; ;q10; ;L
+q9;#;q9;#;R
+q9;(;q9;(;R
+q9;);q9;);R
+q9;A;q9;A;R
+q9;B;q9;B;R
+q9;C;q9;C;R
+q9;D;q9;D;R
+q9;[;q9;[;R
+q9;];q9;];R
+END"""
+print(s)
+
